44ccbb1a8e70cccb33149f723e3e3135f8d95016
lmescoito <lmescoito@hotmail.com>
2024-07-23 22:23:35 -0300

    feat:automate-deploy

--- OneDrive/Documentos/leonan/Profisssão Development Full Stack/projeto final/twitter-app/backend/twitter/urls.py
+++ OneDrive/Documentos/leonan/Profisssão Development Full Stack/projeto final/twitter-app/backend/twitter/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.conf import settings
 from django.conf.urls.static import static
+from twitter import views
 from django.contrib import admin
 from django.urls import path, include
 
@@ -31,6 +32,8 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api-auth/", include("rest_framework.urls")),
+    path("update_server/", views.update, name="update"),
+    path("hello/", views.hello_world, name="hello_world"),
 ]
 
 if settings.DEBUG:
